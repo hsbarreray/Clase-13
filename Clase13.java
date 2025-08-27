97cb8883b1312b6232f48fb8e3165f356e0fce29
hsbarreray <hsbarreray@academia.usbbog.edu.co>
2025-08-26 22:25:15 -0500

    Update Clase13.java

--- Clase13.java
+++ Clase13.java
@@ -1,5 +1,33 @@
-public class Clase13 {
+public class EstructurasEstaticas {
+    public static void main(String[]arg){
+        int a = 10;
+        int b = 11;
+        System.out.println(a);
+        System.out.println(b);
+        long c =300000000;
+        System.out.println(c);
+        int d= a+b;
+        int e= a-b;
+        int f= a*b;
+        float g= c/a;
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        char s= 'h';
+        char t= 'o';
+        char u= 'l';
+        char v= 'a';
+        String suma = ""+s+t+u+v;
+        System.out.println(suma);
+        boolean m=true;
+        boolean ñ=false;
+        System.out.println(m+" "+ñ);
 
-    public static void main(String[] args) 
+        
 
+
+
+
+    }
 }
