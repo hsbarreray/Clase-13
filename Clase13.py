729558ef62a7620bb1b9da9b11f888fc3bf129e2
hsbarreray <hsbarreray@academia.usbbog.edu.co>
2025-08-26 22:24:54 -0500

    Update Clase13.py

--- Clase13.py
+++ Clase13.py
@@ -1,1 +1,16 @@
-a, b = 10, 11
+a = 10
+b = 11
+c = a-b
+d = a/b
+e = a*b
+f = a+b
+print(c)
+print(d)
+print(e)
+print(f)
+palabra= "hola"
+for i in palabra:
+    print(i)
+m= True
+ñ= False
+print(m,ñ)
